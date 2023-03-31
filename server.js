cec43a2d7b3c277533b26ec2b829196f5df2f498
ryansummerlin <ryansummerlin777@gmail.com>
2023-03-30 19:45:43 -0700

    Finished the first 3 phases of the route handlers

--- server.js
+++ server.js
@@ -34,10 +34,53 @@ const server = http.createServer((req, res) => {
 
     /* ======================== ROUTE HANDLERS ========================== */
     // Phase 1: GET /
+    if (req.method === 'GET' && req.url === '/') {
+      newPlayerTemplate = fs.readFileSync('./views/new-player.html', 'utf-8');
+      newPlayerPage = newPlayerTemplate
+        .replace(/#{availableRooms}/g, world.availableRoomsToString());
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      return res.end(newPlayerPage);
+    }
 
     // Phase 2: POST /player
+    if (req.method === 'POST' && req.url === '/player') {
+      let [name, roomId] = [req.body.name, req.body.roomId];
+      player = new Player(name, world.rooms[roomId]);
+
+      res.statusCode = 302;
+      res.setHeader('Location', `/rooms/${roomId}`);
+      return res.end();
+    }
 
     // Phase 3: GET /rooms/:roomId
+    if (req.method === 'GET' && req.url.startsWith('/rooms/')) {
+      const urlParts = req.url.split('/');
+      const roomId = urlParts[2];
+      if (urlParts.length === 3 && world.rooms[roomId]) {
+        const room = player.currentRoom;
+        const roomName = room.name;
+        const roomItems = room.items.map(item => item.name);
+        const inventory = player.items.map(item => item.name);
+        const exits = room.exits;
+        let exitsArr = [];
+        for (let key in exits) {
+          exitsArr.push(key + ": " + exits[key].name);
+        }
+
+        const roomTemplate = fs.readFileSync('./views/room.html', 'utf-8');
+        const roomPage = roomTemplate
+          .replace(/#{roomName}/g, roomName)
+          .replace(/#{roomItems}/g, roomItems.join(', '))
+          .replace(/#{inventory}/g, inventory.join(', '))
+          .replace(/#{exits}/g, exitsArr.join(', '));
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        return res.end(roomPage);
+      }
+    }
 
     // Phase 4: GET /rooms/:roomId/:direction
 
@@ -47,6 +90,6 @@ const server = http.createServer((req, res) => {
   })
 });
 
-const port = 5000;
+const port = 3000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
